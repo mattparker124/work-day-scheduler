d4f7026a25ff43e40f6e1b07f5ec3fe5b98b9eb9
Matthew Parker <mattparker124@gmail.com>
2022-03-20 19:13:24 -0500

    add time tracking for each row

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,41 @@
 $("#currentDay").append(moment().format("dddd, MMMM Do YYYY"));
 
 let renderScheduleContainer = function() {
+    // our work day starts at 9am
     let currentBlock = 9;
     for (let i = 0; i < 9; i++) {
-        let timeBlockLi = $("<p>").attr("id", currentBlock).addClass("row time-block");
+        // create the elements that will make up each timeblock row
+        let timeBlockP = $("<p>").attr("id", currentBlock).addClass("row time-block");
         let hourP = $("<h3>").addClass("col-2 hour").text(moment({hour: currentBlock}).format("LT"))
-        let timeP = $("<div>").addClass("col-9 row").text("TEST");
+        let textP = $("<card>").addClass("col-9 row").text("TEST");
         let saveButton = $("<btn>").attr("id", currentBlock).addClass("col-1 saveBtn").html("<span class='oi oi-file'></span>SAVE");
-        timeBlockLi.append(hourP, timeP, saveButton);
-        $("#timeBlocksCont").append(timeBlockLi);
+
+        //append the three row elements to the timeblock
+        timeBlockP.append(hourP, textP, saveButton);
+
+        //append the timeblock to the timeblocks container
+        $("#timeBlocksCont").append(timeBlockP);
+
+        //check the time of this block
+        auditList(timeBlockP);
+
+        //increment so we can make the next block, or finish
         currentBlock++
+    }
+}
+
+let auditList = function(timeBlock) {
+    let time = $(timeBlock).find("h3").text().trim();
+    let timeMoment = moment(time, "HH");
+
+    if (moment().isAfter(timeMoment)) {
+        $(timeBlock).find("card").addClass("past");
+    }
+    else if (Math.abs(moment().diff(timeMoment, "hours")) <= 1) {
+        $(timeBlock).find("card").addClass("present");
+    }
+    else {
+        $(timeBlock).find("card").addClass("future");
     }
 }
 
